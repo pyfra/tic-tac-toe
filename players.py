f11a878a8e1d7b7f0e20cedf7c9550d52a7956d0
pyfra <f.defano1@gmail.com>
2020-03-08 14:42:02 +0100

    implemented mixed player

--- players.py
+++ players.py
@@ -19,3 +19,22 @@ class HumanPlayer(Player):
 
     def move(self, valid_moves):
         return input("please enter the move: ")
+
+
+class MiniMaxPlayer(Player):
+    pass
+
+
+class MixedPlayer(Player):
+    """
+    A class that will use the random player with probability 1-p and another (better) player with probability p.
+    It will be used to smooth the better player and have intermediate levels of difficulties between the two players
+    """
+
+    def __init__(self, p, other_player):
+        self.p = p
+        self.players = [RandomPlayer(), other_player]
+
+    def move(self, valid_moves):
+        i = np.random.binomial(1, self.p)
+        return self.players[i]
