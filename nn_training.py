a57113a433a7709657ff739275de993c9f6a8b00
pyfra <f.defano1@gmail.com>
2020-03-27 21:23:31 +0100

    complete training nn on tic-tac-toe dataset

--- nn_training.py
+++ nn_training.py
@@ -1,20 +1,94 @@
 import pandas as pd
 import os
-"""
-The outcome are based on X winning
-"""
-# read data and prepare
-# read data
+import numpy as np
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+from sklearn.model_selection import train_test_split
+
+######### Read and prepare the data
 dir_path = os.path.dirname(os.path.realpath(__file__))
 data_raw = pd.read_csv(os.path.join(dir_path, 'dataset', 'tic-tac-toe.data'))
 
-X = data_raw.iloc[:, :9]
-
-
-y = data_raw.iloc[:, 9:]
-
-from sklearn.preprocessing import LabelEncoder, OneHotEncoder
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import confusion_matrix
+# Define and prepare y
+y = data_raw['positive']
 labelencoder_y = LabelEncoder()
-y[:, 0] = labelencoder_y.fit_transform(y[:, 0])
+y = labelencoder_y.fit_transform(y)
+
+# Define and prepare X
+X = data_raw.iloc[:, :9]
+X = OneHotEncoder().fit_transform(X).toarray()
+
+# Train/test split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+
+from keras.models import Sequential
+from keras.layers import Dense, ReLU, Activation, LeakyReLU, Dropout
+
+# Initialize neural network
+nn = Sequential()
+
+# Add first hidden layer (and input layer)
+nn.add(Dense(units=9, kernel_initializer='glorot_normal', input_dim=27))
+nn.add(Activation('tanh'))
+nn.add(Dropout(.5))
+
+# Add second hidden layer
+nn.add(Dense(units=5, kernel_initializer='glorot_normal'))
+nn.add(Activation('tanh'))
+nn.add(Dropout(.5))
+
+
+# Add second hidden layer
+nn.add(Dense(units=3, kernel_initializer='glorot_normal'))
+nn.add(Activation('tanh'))
+
+# Add output layer
+nn.add(Dense(units=1, kernel_initializer='glorot_normal'))
+nn.add(Activation('sigmoid'))
+
+# Compile network
+nn.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+
+from keras.models import Sequential
+from keras.layers import Dense, Activation, Dropout
+
+# Initialize neural network
+nn = Sequential()
+
+# Add first hidden layer (and input layer)
+nn.add(Dense(units=9, kernel_initializer='glorot_normal', input_dim=27))
+nn.add(Activation('tanh'))
+nn.add(Dropout(.5))
+
+# Add second hidden layer
+nn.add(Dense(units=5, kernel_initializer='glorot_normal'))
+nn.add(Activation('tanh'))
+nn.add(Dropout(.5))
+
+
+# Add second hidden layer
+nn.add(Dense(units=3, kernel_initializer='glorot_normal'))
+nn.add(Activation('tanh'))
+
+# Add output layer
+nn.add(Dense(units=1, kernel_initializer='glorot_normal'))
+nn.add(Activation('sigmoid'))
+
+# Compile network
+nn.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
+# Train network
+
+# Train network
+nn.fit(X_train, y_train, batch_size=20, epochs=100)
+
+# score on validation test
+score = nn.evaluate(X_test, y_test)
+_, accuracy = nn.evaluate(X_test, y_test)
+
+print('accuracy on validation test %.4f' % accuracy)
+
+##### Save model
+print('Saving model...')
+import os
+
